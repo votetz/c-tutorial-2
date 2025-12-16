b5772cae906403c67a2877196d803bf737973013
Bogdan Poruchnyk <bogomdannyibogdan@example.com>
2025-12-16 20:19:51 +0200

    Lab03

--- apps/lab03/lab03_1/src/app.c
+++ apps/lab03/lab03_1/src/app.c
@@ -1,102 +1,47 @@
 #include "app.h"
 
-/**
-* char
-* int - short int (short), long int (long) , long long int (long long) C11
-* float
-* double, long double (C11)
-* void
-* 
-* char*
-* short*
-* int*
-* long*
-* float*
-* double*
-* void*
-*/
 
-/*
-* Arrays
-* |####|
-*               |####|
-*   |####|
-*               |####|
-* 
-* |####|####|####|####|####|
-*/
-
-#define MAX_SIZE 100
 int main()
 {
-    //int a0 = -1, a1 = 3, a2 = 5, a3 = 2, a4 = 7;
-    printf("===================== Declare and print simple array\n");
+    double a = -6.0, b = 6.0, h = 0.3, x;
+    double min_y, max_y;
+    int first = 1;
 
+    printf("x\t\ty\n");
+
+    for (x = a; x <= b; x += h)
     {
-        int arr2[5];
+        double y;
 
-        arr2[0] = -1;
-        arr2[1] = 3;
-        arr2[2] = 5;
-        arr2[3] = 2;
-        arr2[4] = 7;
-    }
-
-    {
-        char str[5] = { 'H', 'e', 'l', 'o' };// \0
-        int arr2[6] = {-1, 3, 5, 2, 7};
-
-        for (int i = 0; i < 6; ++i) {
-            printf("%d\t", arr2[i]);
-        }
-        printf("\n");
-    }
-
-    printf("===================== Magic number of array\n");
-
-    {
-        char str[] = { 'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!'};
-        int arr2[] = { -1, 3, 5, 2, 7, 4, -5, 22, 13, 11, 17, 15, -4, 12 };
-
-        printf("sizeof(str)=%lu, sizeof(arr2)=%lu\n", sizeof(str), sizeof(arr2));
-
-        int sz = sizeof(arr2) / sizeof(arr2[0]);
-
-        for (int i = 0; i < sz; ++i) {
-            printf("%d\t", arr2[i]);
-        }
-
-        printf("\n");
-        
-        for (int i = 0; i < sizeof(str)/sizeof(str[0]); ++i) {
-            printf("%c", str[i]);
-        }
-
-        printf("\n");
-    }
-
-    printf("===================== Random value for array\n");
-
-    {
-        int array[MAX_SIZE];
-
-        int sz = 20;
-
-        srand(time(NULL));
-
-        int min = -100, max = 200;
-
-        for (int i = 0; i < sz; ++i)
+        if (x <= 0)
         {
-            array[i] = rand() % (max - min + 1) + min;
+            y = log(x + sqrt(x * x + 1));
+        }
+        else if (x > 0 && x < 5)
+        {
+            y = sin(x + 2.0 / (x - 1.0));
+        }
+        else
+        {
+            y = -x + 2;
         }
 
-        for (int i = 0; i < sz; ++i)
+        printf("%.3f\t%.3f\n", x, y);
+
+        if (first)
         {
-            printf("a[%d]=%d\n", i, array[i]);
+            min_y = y;
+            max_y = y;
+            first = 0;
+        }
+        else
+        {
+            if (y < min_y) min_y = y;
+            if (y > max_y) max_y = y;
         }
     }
 
+    printf("\nMin: %.3f\tMax: %.3f\n", min_y, max_y);
 
     return 0;
 }
--- apps/lab03/lab03_2/src/app.c
+++ apps/lab03/lab03_2/src/app.c
@@ -1,83 +1,59 @@
 #include "app.h"
 
-#define MAX_SIZE 100
-
-/*
-* search item
-* search min, max
-* sort
-* norm
-* slice
-*/
 int main()
 {
-    printf("===================== Random value for array\n");
+ 
+    double a = -4.0, b = 4.0, hx = 0.2, c = 0.0, d = 3.0, hy = 0.1;
 
+    double x, y, z;
+    double min_z, max_z;
+    int first = 1;
+
+    printf("y\\x\t");
+
+    for (x = a; x <= b; x += hx)
     {
-        int array[MAX_SIZE];
-
-        int sz = 20;
-
-        srand(time(NULL));
-
-        int min = -100, max = 200;
-
-        for (int i = 0; i < sz; ++i)
-        {
-            array[i] = rand() % (max - min + 1) + min;
-        }
-
-        for (int i = 0; i < sz; ++i)
-        {
-            printf("a[%d]=%d\n", i, array[i]);
-        }
-
-        {
-            printf("==================Search min, max\n");
-
-            int min = array[0], max = min;
-
-            for (int i = 1; i < sz; ++i)
-            {
-                if (array[i] < min)
-                {
-                    min = array[i];
-                }
-                else if (array[i] > max)
-                {
-                    max = array[i];
-                }
-            }
-
-            printf("Min=%d, max=%d\n", min, max);
-        }
-
-        {
-            printf("==================Search index of key\n");
-
-            int index = -1, key = 51;
-
-            for (int i = 0; i < sz; ++i)
-            {
-                if (array[i] == key)
-                {
-                    index = i;
-                    break;
-                }
-            }
-
-            if (index < 0)
-            {
-                printf("Key %d was not found\n", key);
-            }
-            else {
-                printf("Key %d was found at %d\n", key, index);
-            }
-
-            
-        }
+        printf("%.3f\t", x);
     }
 
+    printf("\n");
+
+    for (y = c; y <= d; y += hy)
+    {
+        printf("%.3f\t", y);
+
+        for (x = a; x <= b; x += hx)
+        {
+      
+            if ((x + y) <= 0)
+            {
+                printf("nan\t");
+            }
+            else
+            {
+                z = log(x + y) / exp(x - y);
+                printf("%.3f\t", z);
+
+                if (first)
+                {
+                    min_z = z;
+                    max_z = z;
+                    first = 0;
+                }
+                else
+                {
+                    if (z < min_z) min_z = z;
+                    if (z > max_z) max_z = z;
+                }
+            }
+        }
+
+        printf("\n");
+    }
+
+    if (!first) {
+        printf("\nMin: %.3f\tMax: %.3f\n", min_z, max_z);
+    }
 
     return 0;
 }
